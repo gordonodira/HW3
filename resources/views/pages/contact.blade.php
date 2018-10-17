e0598feb9196bb697524c65d458f46166e1dd734
gordonodira <go37@njit.edu>
2018-10-17 15:08:00 -0400

    1.Initial Laravel set up- 2.Resources folder:layouts,includes and pages 3.Jsfooter file and head 4.default file 5.about page 6.Contact page 7.Contact form 8.Contact Authentification

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -9,14 +9,15 @@
     <form action="/contact" , method="post">
         {{csrf_field()}}
         <div class="form-group">
+
             <label for="email">Email address</label>
             <input name ="email" type="email" class="form-control" id="email" placeholder="name@example.com">
         </div>
 
         <div class="form-group">
-            <label for="Body">Contact Message</label>
+            <label for="body">Contact Message</label>
 
-            <textarea  Class="form-control" id="Body" rows="3"></textarea>
+            <textarea name="body" Class="form-control" id="body" rows="3"></textarea>
         </div>
         <button type="submit" class="btn btn-primary mb-2">Submit</button>
     </form>
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('pages.index');
 });
@@ -20,8 +9,13 @@ Route::get('/about', function () {
 Route::get('/contact', function () {
     return view('pages.contact');
 });
-Route::post('/contact',function(){
-$data=request()->all();
-dd($data);
+
+Route::post('/contact', function () {
+
+    $data= request()->all();
+    echo "Email :" .$data['email'],'<br>';
+    echo  "Body  :" .$data['body'] ;
+
+
 
 });
