456db55ed6a2c1c257c94dd6d02912b7d34b7e5c
gordonodira <go37@njit.edu>
2018-10-17 11:22:56 -0400

    1.Initial Laravel set up- 2.Resources folder:layouts,includes and pages

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,14 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.index');
+});
+Route::get('/about', function () {
+    return view('pages.about');
+});
+Route::get('/contact', function () {
+    return view('pages.contact');
+});
+Route::get('/menu', function () {
+    return view('pages.menu');
 });
