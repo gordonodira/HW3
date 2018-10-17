7a3ff2b28778e645d5fb8046caa46354ffbb2425
gordonodira <go37@njit.edu>
2018-10-17 13:03:31 -0400

    1.Initial Laravel set up- 2.Resources folder:layouts,includes and pages 3.Jsfooter file and head 4.default file 5.about page 6.Contact page

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,13 +2,20 @@
 <html lang="en">
 @include('includes.head');
 <body>
-@include('includes.menu');
 
-<main role="main">
-  @yield('content')
-</main>
-@include('includes.jsfooter');
-@include('includes.footer');
+<div class="container">
+    <header>
+        @include('includes.menu');
+    </header>
 
+
+    <main role="main">
+        @yield('content')
+    </main>
+    @include('includes.jsfooter');
+    <footer>
+        @include('includes.footer');
+    </footer>
+</div>
 </body>
 </html>
