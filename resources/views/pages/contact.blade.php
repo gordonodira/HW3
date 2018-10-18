a421e67f59513ad95119447f9efee943af624c4b
gordonodira <go37@njit.edu>
2018-10-18 15:48:34 -0400

    1.Initial Laravel set up- 2.Resources folder:layouts,includes and pages 3.Jsfooter file and head 4.default file 5.about page 6.Contact page 7.Contact form 8.Contact Authentification procfile-

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,25 +1,24 @@
-
-/@extends('layouts.default')
+@extends('layouts.default')
 
 @section('content')
-
-    <h1>Contact Information</h1>
-    <p class="lead">Please use this page to contact the site owner.</p>
-
-    <form action="/contact" , method="post">
-        {{csrf_field()}}
-        <div class="form-group">
-
-            <label for="email">Email address</label>
-            <input name ="email" type="email" class="form-control" id="email" placeholder="name@example.com">
+    <div class="card at-3 pa-2 pr-2">
+        <div class="card-title"><h1>Contact</h1>
+            <p class="lead">Please use this page to contact the site owner.</p></div>
+        <div class="card-body">
+            <h1>Contact Information</h1>
+            <p class="lead">Please use this page to contact the site owner.</p>
+            <form action="/contact" , method="post">
+                {{csrf_field()}}
+                <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input name="email" type="email" class="form-control" id="email" placeholder="name@example.com">
+                </div>
+                <div class="form-group">
+                    <label for="body">Contact Message</label>
+                    <textarea name="body" Class="form-control" id="body" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary mb-2">Submit</button>
+            </form>
         </div>
-
-        <div class="form-group">
-            <label for="body">Contact Message</label>
-
-            <textarea name="body" Class="form-control" id="body" rows="3"></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary mb-2">Submit</button>
-    </form>
-
+    </div>
 @endsection;
